4830406ac47a5c2ec5753131ade1b1abd1caa381
C <earthhominid@gmail.com>
2022-01-27 15:33:02 -0300

    formatted ifStatement

--- ifStatement.js
+++ ifStatement.js
@@ -1,4 +1,8 @@
 function counter(state, action) {
+  if (typeof state === 'undefined') {
+    return 0;
+  }
+
   if (action.type === 'INCREMENT') {
     return state + 1;
   } else if (action === 'DECREMENT') {
