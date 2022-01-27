47129170c476bf3f93382c18a3cb78e10a8807a9
C <earthhominid@gmail.com>
2022-01-27 18:35:21 -0300

    bracket formatting

--- createStoreFromScratch.js
+++ createStoreFromScratch.js
@@ -17,9 +17,19 @@ const createStore = (reducer) => {
 
   const getState = () => state;
 
-  const dispatch = (action) => {};
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
 
-  const subscribe = (listener) => {};
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  dispatch({});
 
   return { getState, dispatch, subscribe };
 };
